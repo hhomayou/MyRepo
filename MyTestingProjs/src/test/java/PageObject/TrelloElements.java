44e7aaff8a25676aecdd0e794bb6a0c216ddae5b
Hooman <hhomayounfar@qaconsultants.com>
2017-09-25 17:56:21 -0400

    ok

--- MyTestingProjs/src/test/java/PageObject/TrelloElements.java
+++ MyTestingProjs/src/test/java/PageObject/TrelloElements.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -240,6 +242,11 @@ public class TrelloElements {
 
 	// Board card [
 	public List<WebElement> getCards() { // Get cards of the current board
+		wait.until(new ExpectedCondition<Boolean>() {
+			public Boolean apply(WebDriver driver) {
+				return boardList.findElements(By.xpath("//a[@class='list-card js-member-droppable ui-droppable']")).size() > 0;
+			}
+		});									
 		return boardList.findElements(By.xpath("//a[@class='list-card js-member-droppable ui-droppable']"));
 	}
 	public WebElement getCard() {
